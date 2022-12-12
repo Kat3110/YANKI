2371b4dc294dc6cf0dbe60f1e8404d6a45e836a1
Kat3110 <87698261+Kat3110@users.noreply.github.com>
2022-12-13 00:23:18 +0600

    add catalog

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,21 @@
 import style from "./style.css";
 import SvgLike from "../SvgComponent/SvgComponentLike";
-import Coat from "../../assets/coat.png"
-import FurCoats from "../../assets/FurCoats.png"
-import Jacket from "../../assets/jackets.png"
-import Parks from "../../assets/parks.png"
 
-
-const Card = () => (
+const Card = (props) => (
     <>
         <div className={style.cardBox}>
             <button className={style.cardBoxFavourites} type="button">
                 <SvgLike />
             </button>
-            <img src={Jacket} alt="куртка" className={style.cardBoxImg}/>
+            <img src={props.img} alt="куртка" className={style.cardBoxImg} />
             <div className={style.cardBoxGroup}>
-                <h2 className={style.cardBoxTitle}>Белая куртка</h2>
-                <p className={style.cardBoxPrice}>2900 грн</p>
+                <h2 className={style.cardBoxTitle}>{props.name}</h2>
+                <p className={style.cardBoxPrice}>{props.cost}</p>
                 <ul className={style.cardBoxList}>
+
+                    <li className={style.cardBoxItem}>
+                        <button className={style.cardBoxItemBtn}>XXS</button>
+                    </li>
                     <li className={style.cardBoxItem}>
                         <button className={style.cardBoxItemBtn}>XXS</button>
                     </li>
@@ -34,9 +33,9 @@ const Card = () => (
                     </li>
                 </ul>
                 <div className={style.cardColors}>
-                    <button className="cardColorsSelector test"></button>
-                    <button className={style.cardColorsSelector}></button>
-                    <button className={style.cardColorsSelector}></button>
+                    <button id="ColorWhite" className={style.cardColorsSelector} />
+                    <button id="ColorBlue" className={style.cardColorsSelector} />
+                    <button id="ColorYellow" className={style.cardColorsSelector} />
                 </div>
             </div>
         </div>
--- src/routes/catalog/index.js
+++ src/routes/catalog/index.js
@@ -2,14 +2,96 @@ import {h} from 'preact';
 import style from './style.css'
 import Header from "../../components/header";
 import Card from "../../components/Card";
+import Coat2 from "../../assets/catalog/coat2.png"
+import FurCoats2 from "../../assets/catalog/FurCoats2.png"
+import Jacket2 from "../../assets/catalog/jackets2.png"
+import Parks2 from "../../assets/catalog/parks2.png"
 
 
 const Catalog = () => (
     <>
         <Header />
         <section className={style.catalog}>
-            <div className={style.box}>
-    <Card />
+            <div className={style.catalogBox}>
+                <div className={style.catalogList}>
+                    <a href="#" target="_blank">New
+                    </a>
+                    <a href="#" target="_blank">Bestsellers</a>
+                    <a href="#" target="_blank">Верхняя одежда
+                    </a>
+                    <a href="#" target="_blank">Шубы</a>
+                    <a href="#" target="_blank">Тренчи
+                    </a>
+                    <a href="#" target="_blank">Пальто</a>
+                    <a href="#" target="_blank">Пуховики и жилеты
+                    </a>
+                    <a href="#" target="_blank">Sale</a>
+                    <a href="#" target="_blank">Посмотреть всё</a>
+                </div>
+                <div className={style.cards}>
+                    <Card
+                        img={Jacket2}
+                        name={'Белая куртка'}
+                        cost={'2900 грв'}
+                    />
+                    <Card
+                        img={Coat2}
+                        name={'Синее пальто'}
+                        cost={'3150 грв'}
+                    />
+                    <Card
+                        img={FurCoats2}
+                        name={'Бежевая шуба'}
+                        cost={'4200 грв'}
+                    />
+                    <Card
+                        img={Parks2}
+                        name={'Синяя парка'}
+                        cost={'2900 грв'}
+                    />
+
+                    <Card
+                        img={Coat2}
+                        name={'Синее пальто'}
+                        cost={'3150 грв'}
+                    />
+                    <Card
+                        img={Parks2}
+                        name={'Синяя парка'}
+                        cost={'2900 грв'}
+                    />
+                    <Card
+                        img={Jacket2}
+                        name={'Белая куртка'}
+                        cost={'2900 грв'}
+                    />
+                    <Card
+                        img={FurCoats2}
+                        name={'Бежевая шуба'}
+                        cost={'4200 грв'}
+                    />
+
+                    <Card
+                        img={FurCoats2}
+                        name={'Бежевая шуба'}
+                        cost={'4200 грв'}
+                    />
+                    <Card
+                        img={Jacket2}
+                        name={'Белая куртка'}
+                        cost={'2900 грв'}
+                    />
+                    <Card
+                        img={Parks2}
+                        name={'Синяя парка'}
+                        cost={'2900 грв'}
+                    />
+                    <Card
+                        img={Coat2}
+                        name={'Синее пальто'}
+                        cost={'3150 грв'}
+                    />
+                </div>
             </div>
         </section>
     </>
