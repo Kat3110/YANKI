7731da8994b03427d2620f8c57f6c72096f796c1
Kat3110 <87698261+Kat3110@users.noreply.github.com>
2022-12-12 10:33:48 +0600

    animation

--- src/components/subscribe/index.js
+++ src/components/subscribe/index.js
@@ -1,13 +1,15 @@
-
 import style from './style.css'
 
 const Subscribe = () => (
     <section className={style.subscribe}>
         <h2 className={style.title}>Узнайте первым о новинках</h2>
-        <input className={style.input} type="text" placeholder='Ваш e-mail*'/>
+        <div className={style.inputGroup}>
+            <input className={style.input} type="text" />
+            <label className={style.label}>Ваш e-mail*</label>
+        </div>
         <button className={style.btn} type='button'>Подписаться</button>
         <p className={style.subtitle}>
-            Нажимая на кнопку «Подписаться», я соглашаюсь на обработку моих< br/>
+            Нажимая на кнопку «Подписаться», я соглашаюсь на обработку моих<br />
             персональных данных и ознакомлен(а) с условиями конфиденциальности.
         </p>
     </section>
